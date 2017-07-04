b5994ccff9f077558d6908ae219e38773b1edd02
initcole <cp110@protonmail.com>
2017-07-04 11:32:36 -0700

    Adding var name.rb

--- Variables/name.rb
+++ Variables/name.rb
@@ -7,3 +7,19 @@ puts "Nice to meet you #{first_name} #{last_name}"
   puts first_name
   puts last_name
 end
+
+10.times do
+  puts "cole"
+end
+
+#10.times do |name|
+#  name = gets.chomp
+#  puts "My name is #{name}"
+#end  
+
+
+puts "What is your first name?"
+first_name = gets.chomp
+puts "Thank you. What is your last name?"
+last_name = gets.chomp
+puts "Great. So your full name is " + first_name
